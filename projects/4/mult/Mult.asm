3f387b921226c2e7e08dbef79c8ca6f1ef73def3
Lucas Black <lucasbblack.work@gmail.com>
2025-05-26 16:13:43 -0700

    project 4 - multiplication in assembly

---
+++ projects/4/mult/Mult.asm
@@ -0,0 +1,48 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// The algorithm is based on repetitive addition.
+
+@R0
+D=M
+@CHECKZERO
+D;JLE
+@R1
+D=M
+@CHECKZERO
+D;JLE
+@R2
+M=D
+@R0
+D=M
+D=D-1
+@LOOP
+D;JGT
+@END
+D;JLE
+
+(LOOP)
+	@temp
+	M=D
+	@R1
+	D=M
+	@R2
+	M=D+M
+	@temp
+	D=M
+	D=D-1
+	@LOOP
+	D;JGT
+	@END
+	D;JLE
+
+(CHECKZERO)
+	@R2
+	M=0
+	@END
+	0;JMP
+
+(END)
