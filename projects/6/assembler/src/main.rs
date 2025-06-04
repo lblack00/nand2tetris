8927298a9e793ac676bcbff6403b20faa3c8ff94
Lucas Black <lucasbblack.work@gmail.com>
2025-06-03 20:49:40 -0700

    project 6 - WIP symbol table

--- projects/6/assembler/src/main.rs
+++ projects/6/assembler/src/main.rs
@@ -1,5 +1,7 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
+use regex::Regex;
 
 #[derive(Clone, Debug, PartialEq)]
 enum InstructionType {
@@ -10,37 +12,76 @@ enum InstructionType {
 
 struct Parser<'a> {
     filepath: &'a str,
+    symbols: &'a mut HashMap<String, usize>,
 }
 
 impl Parser<'_> {
     pub fn parse(&mut self) {
+        let mut symbol_counter: usize = 16;
         if let Ok(lines) = self.read_lines() {
             // Consumes the iterator, returns an (Optional) String
             for line in lines.map_while(Result::ok) {
-                let current_instruction = match line.split_once("//") {
-                    Some((before, _)) => before.trim(),
-                    None => line.trim(),
+                self.parse_line(&line, &mut symbol_counter);
+            }
+        }
+    }
+
+    // todo: clean this up
+    fn parse_line(&mut self, line: &str, symbol_counter: &mut usize) -> bool {
+        // Take instructions before inline comments
+        let current_instruction = match line.split_once("//") {
+            Some((before, _)) => before.trim(),
+            None => line.trim(),
+        };
+        // Check if instruction is whitespace or comment
+        if current_instruction.is_empty() {
+            return false;
+        }
+
+        // Determine instruction type
+        let instruction_type: InstructionType = self.instruction_type(current_instruction);
+
+        // Parse instructions
+        if instruction_type == InstructionType::CInstruction {
+            let dest_str = self.dest(current_instruction);
+            let comp_str = self.comp(current_instruction);
+            let jump_str = self.jump(current_instruction);
+            let bin_str = "111".to_owned() + dest_str + comp_str + jump_str;
+            println!("{:?}", bin_str);
+        } else if instruction_type == InstructionType::AInstruction {
+            let addr_str = self.addr(current_instruction);
+
+
+            if !addr_str.is_empty() {
+                let re = Regex::new(r"^[A-Z]+_*[0-9]*$").unwrap();
+                let Some(symbol) = re.captures(addr_str) else {
+                //     // let bin_str =
+                //     //     format!("{:016b}", addr_str.parse::<usize>().unwrap() & 0x7FFF);
+                //     // // println!("{:?}", bin_str);
+                //     // return true;
+                    return true;
                 };
 
-                if current_instruction.is_empty() {
-                    continue;
+                // println!("{:?} {:?}", symbol, addr_str);
+                // );
+                if !self.symbols.contains_key(&symbol[0]) {
+                    self.symbols.insert(symbol[0].to_string(), *symbol_counter);
+                    *symbol_counter += 1;
                 }
 
-                let instruction_type: InstructionType = self.instruction_type(current_instruction);
-
-                if instruction_type == InstructionType::CInstruction {
-                    let dest_str = self.dest(current_instruction);
-                    let comp_str = self.comp(current_instruction);
-                    let jump_str = self.jump(current_instruction);
-                    println!("{:?} {:?} {:?}", dest_str, comp_str, jump_str);
-                } else if instruction_type == InstructionType::AInstruction {
-                    // let address = &line_without_comments[1..].parse::<usize>().unwrap() & 0x7FFF;
-                    // let bin_address = format!("{:015b}", address);
-                    // println!("{:?}", bin_address);
-                }
-
-                println!("{:?} {:?}", current_instruction, instruction_type);
+                print!("{:?}", self.symbols.get(&symbol[0]));
             }
+        }
+
+        println!("{:?} {:?}", current_instruction, instruction_type);
+
+        true
+    }
+
+    fn addr<'a>(&self, instruction: &'a str) -> &'a str {
+        match instruction.split_once('@') {
+            Some((_, addr_str)) => addr_str,
+            None => "",
         }
     }
 
@@ -143,8 +184,24 @@ impl Parser<'_> {
 }
 
 fn main() {
+    let mut default_symbols = HashMap::<String, usize>::from([
+        ("SP".to_string(), 0x0000),
+        ("LCL".to_string(), 0x0001),
+        ("ARG".to_string(), 0x0002),
+        ("THIS".to_string(), 0x0003),
+        ("THAT".to_string(), 0x0004),
+        ("SCREEN".to_string(), 0x4000),
+        ("KBD".to_string(), 0x6000),
+    ]);
+    // Default registers R0..R15
+    for i in 0..15 {
+        let formatted_symbol = format!("R{:}", i).to_string();
+        default_symbols.insert(formatted_symbol, i);
+    }
+
     let mut parser = Parser {
-        filepath: "../add/Add.asm",
+        filepath: "../max/Max.asm",
+        symbols: &mut default_symbols,
     };
     // Call parser to translate .asm file into binary
     parser.parse();
